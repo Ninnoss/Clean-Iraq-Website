5d0b1e92e1fb4d1edf1f2c563e8b043fd518fbbc
Ninos <ninos.dinkha97@gmail.com>
2023-08-04 15:51:34 +0300

    finished Navbar

--- src/components/Navigation/MobileNavbar.jsx
+++ src/components/Navigation/MobileNavbar.jsx
@@ -1,5 +1,73 @@
-const MobileNavbar = () => {
-  return <nav></nav>;
+/* eslint-disable react/prop-types */
+import { NavLink } from 'react-router-dom';
+import { AiOutlineClose } from 'react-icons/ai';
+
+const MobileNavbar = ({ setToggleMenu }) => {
+  return (
+    <nav
+      role="dialog"
+      aria-modal="true"
+      className="fixed top-0 left-0 w-full h-full bg-highlightGrey flex justify-center items-center slide-bottom z-10  ">
+      <AiOutlineClose
+        size={'2.5rem'}
+        className="fixed top-5 right-5 text-highlightBlack hover:text-primaryGreen "
+        onClick={() => setToggleMenu(false)}
+        aria-label="Close Menu"
+      />
+      <ul
+        role="menu"
+        className="flex flex-col items-center justify-center  space-y-5 text-2xl md:text-3xl  text-highlightBlack font-arabicMedium ">
+        <li className="hover:text-primaryGreen tracking-wider">
+          <NavLink
+            onClick={() => setToggleMenu(false)}
+            to={'/'}
+            role="menuitem">
+            الرئيسية
+          </NavLink>
+        </li>
+        <li className="hover:text-primaryGreen tracking-wider">
+          <NavLink
+            onClick={() => setToggleMenu(false)}
+            to={'/about'}
+            role="menuitem">
+            من نحن
+          </NavLink>
+        </li>
+        <li className="hover:text-primaryGreen tracking-wider">
+          <NavLink
+            onClick={() => setToggleMenu(false)}
+            to={'/campagins'}
+            role="menuitem">
+            حملاتنا
+          </NavLink>
+        </li>
+        <li className="hover:text-primaryGreen tracking-wider">
+          <NavLink
+            onClick={() => setToggleMenu(false)}
+            to={'/projects'}
+            role="menuitem">
+            مبادراتنا
+          </NavLink>
+        </li>
+        <li className="hover:text-primaryGreen tracking-wider">
+          <NavLink
+            onClick={() => setToggleMenu(false)}
+            to="/blogs"
+            role="menuitem">
+            المدونة
+          </NavLink>
+        </li>
+        <li className="hover:text-primaryGreen tracking-wider">
+          <NavLink
+            onClick={() => setToggleMenu(false)}
+            to={'/contact'}
+            role="menuitem">
+            اتصل بنا
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default MobileNavbar;
--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,7 +1,72 @@
+import { NavLink } from 'react-router-dom';
+import images from '../../data/images';
+import Button from '../Button';
+import { HiMenuAlt2 } from 'react-icons/hi';
+import { useState } from 'react';
+import MobileNavbar from './MobileNavbar';
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <header>
-      <nav></nav>
+      <nav className="flex justify-between md:justify-around items-center px-4 py-2 border-b-[#DDE1E6] border-[1px]">
+        <div>
+          <NavLink
+            to={'/'}
+            end>
+            <div className="flex items-center">
+              <figure>
+                <img
+                  className="w-[50px] sm:w-[75px] lg:w-[100px]"
+                  src={images.navLogo}
+                  alt="Clean Iraq Campaign Logo"
+                />
+                <figcaption className="sr-only">Clean Iraq Campaign Logo</figcaption>
+              </figure>
+              <span className="text-md lg:text-xl text-black font-arabicMedium  md:font-arabicSemibold  mx-3 ">سفراء النظافة</span>
+            </div>
+          </NavLink>
+        </div>
+
+        {/* Mobile Menu */}
+        {toggleMenu && <MobileNavbar setToggleMenu={setToggleMenu} />}
+
+        {/* Desktop Menu */}
+        <div className="hidden md:block">
+          <ul className="flex gap-x-4 lg:gap-x-10  text-lg text-highlightBlack font-arabicMedium">
+            <li className="hover:text-primaryGreen">
+              <NavLink
+                to="/"
+                end>
+                الرئيسية
+              </NavLink>
+            </li>
+            <li className="hover:text-primaryGreen ">
+              <NavLink to="/about">من نحن</NavLink>
+            </li>
+            <li className="hover:text-primaryGreen ">
+              <NavLink to="/campaigns">حملاتنا</NavLink>
+            </li>
+            <li className="hover:text-primaryGreen ">
+              <NavLink to="/projects">مبادراتنا</NavLink>
+            </li>
+            <li className="hover:text-primaryGreen ">
+              <NavLink to="/blogs">المدونة</NavLink>
+            </li>
+            <li className="hover:text-primaryGreen ">
+              <NavLink to="/contact">اتصل بنا</NavLink>
+            </li>
+          </ul>
+        </div>
+        <HiMenuAlt2
+          className="md:hidden"
+          onClick={() => setToggleMenu(true)}
+          size={'1.5rem'}
+          color="#202020"
+          aria-label="Open Menu"
+        />
+        <Button className="rounded-3xl px-3 py-1 lg:px-4 lg:py-2 font-medium hidden md:block ">أنضم الينا</Button>
+      </nav>
     </header>
   );
 };
