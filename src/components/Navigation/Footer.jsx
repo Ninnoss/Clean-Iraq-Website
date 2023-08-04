003543390951345c48751da325636fd97512a2ea
Ninos <ninos.dinkha97@gmail.com>
2023-08-04 16:39:48 +0300

    finished the footer

--- src/components/Navigation/Footer.jsx
+++ src/components/Navigation/Footer.jsx
@@ -1,8 +1,114 @@
+import { NavLink } from 'react-router-dom';
+import { FaFacebook, FaTwitter, FaInstagram, FaYoutube } from 'react-icons/fa';
+import images from '../../data/images';
+
 const Footer = () => {
-//   const scrollToTop = () => {
-//     window.scrollTo(0, 0);
-//   };
-  return <footer></footer>;
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+  return (
+    <footer className="bg-highlightBlack px-2">
+      <div className="mx-auto  w-full max-w-screen-xl p-4 py-6 lg:py-8 flex flex-col gap-y-4 md:flex-row items-center justify-between">
+        <div>
+          <NavLink
+            to={'/'}
+            end>
+            <div className="flex items-center">
+              <figure>
+                <img
+                  className="w-[50px] sm:w-[75px] lg:w-[100px]"
+                  src={images.navLogo}
+                  alt="Clean Iraq Campaign Logo"
+                />
+                <figcaption className="sr-only">Clean Iraq Campaign Logo</figcaption>
+              </figure>
+              <span className="text-md lg:text-xl text-highlightGrey font-arabicMedium  md:font-arabicSemibold mx-3 ">سفراء النظافة</span>
+            </div>
+          </NavLink>
+        </div>
+
+        <div>
+          <ul className="grid grid-cols-3 lg:grid-cols-6 justify-center gap-4 lg:gap-x-10  text-lg text-highlightGrey font-arabicMedium">
+            <li className="hover:text-primaryGreen">
+              <NavLink
+                onClick={scrollToTop}
+                to="/"
+                end>
+                الرئيسية
+              </NavLink>
+            </li>
+            <li className="hover:text-primaryGreen ">
+              <NavLink
+                onClick={scrollToTop}
+                to="/about">
+                من نحن
+              </NavLink>
+            </li>
+            <li className="hover:text-primaryGreen">
+              <NavLink
+                onClick={scrollToTop}
+                to="/campaigns">
+                حملاتنا
+              </NavLink>
+            </li>
+            <li className="hover:text-primaryGreen">
+              <NavLink
+                onClick={scrollToTop}
+                to="/projects">
+                مبادراتنا
+              </NavLink>
+            </li>
+            <li className="hover:text-primaryGreen">
+              <NavLink
+                onClick={scrollToTop}
+                to="/blogs">
+                المدونة
+              </NavLink>
+            </li>
+            <li className="hover:text-primaryGreen">
+              <NavLink
+                onClick={scrollToTop}
+                to="/contact">
+                اتصل بنا
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <div className="flex gap-3 mt-2 text-highlightGrey">
+            <a
+              href="#"
+              className=" hover:scale-110 transition-all duration-200 mx-2"
+              aria-label="Instagram">
+              <FaInstagram />
+            </a>
+            <a
+              href="#"
+              className=" hover:scale-110 transition-all duration-200 mx-2"
+              aria-label="Instagram">
+              <FaYoutube />
+            </a>
+
+            <a
+              href="#"
+              className=" hover:scale-110 transition-all duration-200 mx-2"
+              aria-label="Facebook">
+              <FaFacebook />
+            </a>
+            <a
+              href="#"
+              className=" hover:scale-110 transition-all duration-200 mx-2"
+              aria-label="Twitter">
+              <FaTwitter />
+            </a>
+          </div>
+        </div>
+      </div>
+      <hr className="my-2 border-gray-200/50 mx-auto w-[80vw]" />
+      <small className="text-white block py-4 text-center">© 2023 حملة تنضيف العراق™. كل الحقوق محفوظة.</small>
+    </footer>
+  );
 };
 
 export default Footer;
