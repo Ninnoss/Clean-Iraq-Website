742e8f0f1cb984508bb5e862db98df13e6b37c81
Shahad161 <shahad.hamza.014@gmail.com>
2023-08-06 23:11:14 +0300

    fix some bugs

--- src/components/About_Page/Row.jsx
+++ src/components/About_Page/Row.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
-import { useInView } from "react-intersection-observer";
-const Row = ({ title, description2,description, imageSrc, imageAlt, isLTR }) => {
+import { useInView } from 'react-intersection-observer';
+const Row = ({ title, description2, description, imageSrc, imageAlt, isLTR }) => {
   const [ref, inView] = useInView({
     triggerOnce: true, // Only trigger once when the element enters the viewport
     threshold: 0.2, // The threshold of how much of the element is in view
@@ -13,13 +13,13 @@ const Row = ({ title, description2,description, imageSrc, imageAlt, isLTR }) => 
       ref={ref}
       className={`reveal ${animationClass}`}>
       <div className={`p-7 flex flex-col space-y-8  lg:items-center mx-auto ${!isLTR ? 'md:flex-row' : 'md:flex-row-reverse'}`}>
-        <div className={`sm:w-10/12 md:w-6/12 lg:w-4/12 mx-auto my-8`}>
-          <h3 className="font-markazi text-5xl text-highlightBlack font-semibold pb-12">{title}</h3>
-          <p className="text-justify text-xl text-highlightBlack font-karla mb-6">{description}</p>
-          <p className="text-justify text-xl text-highlightBlack font-karla ">{description2}</p>
+        <div className={`sm:w-10/12 md:w-6/12 lg:w-4/12 mx-auto my-8 text-highlightBlack`}>
+          <h3 className="pb-12 text-Heading-2">{title}</h3>
+          <p className="text-justify mb-6 text-Body-L">{description}</p>
+          <p className="text-justify text-Body-M">{description2}</p>
         </div>
 
-        <figure className={`flex justify-center md:w-1/2  `}>
+        <figure className={`flex justify-center md:w-1/2`}>
           <img
             className={`w-12/12 min-[550px]:w-11/12 sm:w-10/12 lg:w-9/12 rounded-lg lg:relative ${isLTR ? 'lg:top-4' : ''} 'lg:top-10'`}
             src={imageSrc}
