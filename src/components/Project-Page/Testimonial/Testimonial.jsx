290165403c6a9a0163ceff96437ad4a248509389
coffeholic404 <taqealibade@gmail.com>
2023-08-10 14:59:14 +0300

    eslint disable

--- src/components/Project-Page/Testimonial/Testimonial.jsx
+++ src/components/Project-Page/Testimonial/Testimonial.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
 import TestimonialCard from "./TestimonialCard";
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 
