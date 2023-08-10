ead1fca4fdd935ffdf7c28675e9fd29fe73aa697
Humam-Yousif <humam_yaqoob20@yahoo.com>
2023-08-10 16:47:03 +0300

    Finished campaigns page

--- src/components/Campaigns/Form.jsx
+++ src/components/Campaigns/Form.jsx
@@ -1,10 +1,11 @@
 import Button from "../Button";
 
 const Form = () => {
-    return <form className="text-center">
-        <div>
+    return <form className="flex flex-col items-center px-72 py-12 text-Body-L">
+        <div className="w-full py-2">
             <label htmlFor="name">الاسم</label>
             <input 
+                className="bg-gray-100 px-4 py-3 my-2 border-solid border-gray-300 border-b w-full "
                 type="text" 
                 id="name"
                 name="name"
@@ -13,9 +14,10 @@ const Form = () => {
                 //onChange={handleChange}
             />
         </div>
-        <div>
+        <div className="w-full py-2">
             <label htmlFor="age">العمر</label>
             <input 
+                className="bg-gray-100 px-4 py-3 my-2 border-solid border-gray-300 border-b w-full "
                 type="number" 
                 id="age"
                 name="age"
@@ -24,9 +26,10 @@ const Form = () => {
                 //onChange={handleChange}
             />
         </div>
-        <div>
+        <div className="w-full py-2">
             <label htmlFor="number">رقم الهاتف</label>
             <input 
+                className="bg-gray-100 px-4 py-3 my-2 border-solid border-gray-300 border-b w-full "
                 type="number" 
                 id="number"
                 name="number"
@@ -35,10 +38,13 @@ const Form = () => {
                 //onChange={handleChange}
             />
         </div>
-        <div>
+        <div className="w-full py-2">
             <label htmlFor="sex">الجنس</label>
-            <label>
+            <div>
+            <label className="ml-3">
                 <input
+                className="ml-2"
+                style={{'accent-color': '#ce8116'}}
                 type="radio"
                 value="male"
                 id="sex"
@@ -48,9 +54,11 @@ const Form = () => {
                 //onChange={handleOptionChange}
                 />
                 ذكر
-            </label>
-            <label>
+                </label>
+                <label className="mr-3">
                 <input
+                className="ml-2"
+                style={{'accent-color': '#ce8116'}}
                 type="radio"
                 value="female"
                 id="sex"
@@ -59,11 +67,13 @@ const Form = () => {
                 //onChange={handleOptionChange}
                 />
                 انثى
-            </label>
+                </label>
+            </div>
         </div>
-        <div>
+        <div className="w-full py-2">
             <label htmlFor="telegram-id">معرف التيليگرام</label>
             <input 
+                className="bg-gray-100 px-4 py-3 my-2 border-solid border-gray-300 border-b w-full "
                 type="text" 
                 id="telegram-id"
                 name="telegram-id"
@@ -72,28 +82,43 @@ const Form = () => {
                 //onChange={handleChange}
             />
         </div>
-        <div>
+        <div className="w-full py-2">
             <label htmlFor="Occupation">المهنة</label>
-            <select >
+            <select className="bg-gray-100 px-4 py-3 my-2 border-solid border-gray-300 border-b w-full ">
                 <option value="">المهنة</option>
-                <option value="option1">Option 1</option>
-                <option value="option2">Option 2</option>
-                <option value="option3">Option 3</option>
+                <option value="student">طالب</option>
+                <option value="free-businees">كاسب</option>
+                <option value="employee">موظف (قطاع خاص او حكومي)</option>
             </select>
         </div>
-        <div>
+        <div className="w-full py-2">
             <label htmlFor="goverment">المحافظة</label>
-            <p>(مو شرط نفسها محافظة السكن)المحافظة الي راح تشارك بتنظيفها هذا الاسبوع</p>
-            <select >
+            <p className=" text-Body-S">المحافظة الي راح تشارك بتنظيفها هذا الاسبوع (مو شرط نفسها محافظة السكن)</p>
+            <select className="bg-gray-100 px-4 py-3 my-2 border-solid border-gray-300 border-b w-full ">
                 <option value="">المحافظة</option>
-                <option value="option1">Option 1</option>
-                <option value="option2">Option 2</option>
-                <option value="option3">Option 3</option>
+                <option value="بغداد">بغداد</option>
+                <option value="نينوى">نينوى</option>
+                <option value="البصرة">البصرة</option>
+                <option value="القادسية">القادسية</option>
+                <option value="اربيل">اربيل</option>
+                <option value="النجف">النجف</option>
+                <option value="بابل">بابل</option>
+                <option value="الانبار">الانبار</option>
+                <option value="ذي قار">ذي قار</option>
+                <option value="كركوك">كركوك</option>
+                <option value="ميسان">ميسان</option>
+                <option value="السليمانية">السليمانية</option>
+                <option value="دهوك">دهوك</option>
+                <option value="صلاح الدين">صلاح الدين</option>
+                <option value="كربلاء">كربلاء</option>
+                <option value="ديالى">ديالى</option>
+                <option value="المثنى">المثنى</option>
+                <option value="واسط">واسط</option>
             </select>
         </div>
-        <div>
-            <p>املي هذه الاستمارة حتى تحصل على الباركود الخاص بيك وتستخدمه بكل تجمعات سفراء النظافة.</p>
-            <p>ملاحظة: اذا ما تحب تشاركنا معلوماتك، ممكن تحضر بدون ما تملي الاستمارة، بس ما راح تحصل على اي وحدة من مطبوعات الحملة (مثلا تيشيرت، مطارة مي، ميدالية، وغيرها)</p>
+        <div className="w-full text-center">
+            <p className=" py-2 text-Subtitile-M font-bold">املي هذه الاستمارة حتى تحصل على الباركود الخاص بيك وتستخدمه بكل تجمعات سفراء النظافة.</p>
+            <p className=" py-2 text-Subtitile-M font-bold">ملاحظة: اذا ما تحب تشاركنا معلوماتك، ممكن تحضر بدون ما تملي الاستمارة، بس ما راح تحصل على اي وحدة من مطبوعات الحملة (مثلا تيشيرت، مطارة مي، ميدالية، وغيرها)</p>
         </div>
 
         <Button children={'انضم الينا'}/>
