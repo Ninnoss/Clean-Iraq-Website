632b15de09f7d3b49c8eccf1badc5f3d596ac93a
Shahad161 <shahad.hamza.014@gmail.com>
2023-08-19 00:16:09 +0300

    fixed card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,35 +17,18 @@ const Card = ({ name, img, description, trashBags, volunteers, date, location, i
           loading="lazy"
           alt={`Image of ${name}`}
         />
-        <figcaption className="sr-only">{name}</figcaption>
       </figure>
 
-      <div className="px-6">
+      <div className="px-6 text-center">
         <header>
-          <h3 className="my-4 text-Heading-4 text-center tracking-tight">{t(name)}</h3>
+          <h3 className="mt-4 text-Heading-6 text-center tracking-tight">{name}</h3>
         </header>
-        <p className={`mb-3 text-Body-S text-center ${date && location ? 'h-28 md:h-20' : ''}  text-[#777E90]`}>{t(description)}</p>
+        <span className="text-Body-s block text-highlightBlack">{date}</span>
+        <p className={`my-3 text-Body-s text-center line-clamp-3  text-[#777E90]`}>{description}</p>
       </div>
 
       {date && location && (
-        <div className="flex justify-between items-center p-5 date-place">
-          <span className="text-Body-M block text-highlightBlack">{date}</span>
-          <div className="flex items-center gap-3">
-            <img
-              src={pin}
-              alt="pin"
-            />
-            {t(location)}
-          </div>
-        </div>
-      )}
-
-      <h3 className="mt-4 text-Heading-6 text-center tracking-tight">{name}</h3>
-
-      {date && location && (
         <div className="px-6 text-center">
-          <span className="text-Body-s block text-highlightBlack">{date}</span>
-          <p className={`my-3 text-Body-s text-center line-clamp-3  text-[#777E90]`}>{description}</p>
           <div className="flex justify-center text-Body-s gap-3">
             <img
               src={pin}
@@ -66,50 +49,17 @@ const Card = ({ name, img, description, trashBags, volunteers, date, location, i
         </div>
       )}
 
-      {date && trashBags && volunteers && description && (
+      {date && trashBags && volunteers &&  (
         <div>
-          <div className="text-Body-M px-5 pb-3">
-            <span>
-              {t('card.date')}
-              {date}
-            </span>
-            <br />
-            <span>
-              {t('card.volunteers')}
-              {volunteers}
-            </span>
-            <br />
-            <span>
-              {t('card.trash bags')}
-              {trashBags}
-            </span>
-          </div>
-
           <div className="text-center px-5 pb-3">
+           <p className={`my-3 text-Body-s text-center line-clamp-3  text-[#777E90]`}>قام <strong className=' text-highlightBlack'>{volunteers} متطوع </strong> بالمشاركة بهذهِ الحملة, وقمنا بجمع اكثر من <strong className=' text-highlightBlack'>{trashBags} كيس قمامة </strong></p>
             <div className="flex justify-center text-Body-s gap-3">
               <img
                 src={pin}
                 alt="pin"
               />
-              {t(description)}
+              {t(name)}
             </div>
-            <div className="flex justify-center gap-4 py-3">
-              <div className="flex-grow flex-col items-center bg-[#F1F5F3] rounded-xl ">
-                <h1 className="text-Subtitile-M">
-                  {' '}
-                  <strong>{volunteers}</strong>{' '}
-                </h1>
-                <p className="text-Body-XS">{t('card.volunteer')}</p>
-              </div>
-              <div className="flex-grow flex-col items-center bg-[#F1F5F3] rounded-xl ">
-                <h1 className="text-Subtitile-M">
-                  {' '}
-                  <strong>{trashBags}</strong>{' '}
-                </h1>
-                <p className="text-Body-S">{t('card.trash-bag')}</p>
-              </div>
-            </div>
-            <span className="text-Body-XS block text-[#777E90]">{date}</span>
           </div>
         </div>
       )}
