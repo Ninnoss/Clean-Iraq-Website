c57452da76002b91f86f23ede261f1f7b62fd94c
Ninos <ninos.dinkha97@gmail.com>
2023-10-28 16:08:02 +0300

    updated map

---
+++ src/components/Projects-Page/Map/CampaignDetails.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable react/prop-types */
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+
+const CampaignDetails = ({ selectedEvent, currentPhotoIndex, setCurrentPhotoIndex }) => {
+
+  const handleNextPhoto = () => {
+    if (selectedEvent.Photos && currentPhotoIndex < selectedEvent.Photos.length - 1) {
+      setCurrentPhotoIndex(currentPhotoIndex + 1);
+    }
+  };
+
+  const handlePrevPhoto = () => {
+    if (selectedEvent.Photos && currentPhotoIndex > 0) {
+      setCurrentPhotoIndex(currentPhotoIndex - 1);
+    }
+  };
+  
+  return (
+    <div className="flex-col items-center justify-center rounded-lg pr-8 pb-4">
+      <div className="text-center mb-2">
+        <p className="text-lg font-semibold">{selectedEvent.Province}</p>
+        <p className="text-sm text-gray-500">{selectedEvent['Location Description']}</p>
+      </div>
+      {selectedEvent.Photos ? (
+        <div>
+          <img
+            src={selectedEvent.Photos[currentPhotoIndex].full.url}
+            alt="Event"
+            className="w-[300px] h-[120px]  sm:h-[250px] object-cover"
+          />
+          <div
+            className="cursor-pointer absolute top-[55%] -right-1 font-bold text-xl transform -translate-y-1/2 mx-2"
+            onClick={handleNextPhoto}>
+            <FaAngleRight
+              size={25}
+              className="hover:text-primaryGreen"
+              aria-label="Previous Photo"
+            />
+          </div>
+          <div
+            className="cursor-pointer absolute top-[55%] -left-1 font-bold text-xl transform -translate-y-1/2 mx-2"
+            onClick={handlePrevPhoto}>
+            <FaAngleLeft
+              size={25}
+              className="hover:text-primaryGreen"
+              aria-label="Next Photo"
+            />
+          </div>
+        </div>
+      ) : (
+        <p>Campaign photos not available</p>
+      )}
+    </div>
+  );
+};
+
+export default CampaignDetails;
